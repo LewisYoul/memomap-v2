21374108e4f525d67420be07f430b791e3d7b8e5
Lewis Youl <lewisyoul@gmail.com>
2018-05-05 12:04:39 +0100

    [feature] create map - add marker on click

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
 
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  markerToggle: boolean = false;
 
+  constructor(public navCtrl: NavController, public geo: Geolocation) {
+
+  }
+
+  ionViewDidLoad(){
+    console.log("anything")
+    this.loadMap();
+  }
+
+  toggleMarkerDrop() {
+    this.markerToggle = !this.markerToggle
+  }
+
+  addMarker(event){
+    if (this.markerToggle) {
+      console.log(event)
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: event.latLng
+        // icon: 'http://www.myiconfinder.com/uploads/iconsets/256-256-a5485b563efc4511e0cd8bd04ad0fe9e.png'
+      });
+      this.toggleMarkerDrop();
+      let content = "<h4>Information!</h4>";
+
+      this.addInfoWindow(marker, content);
+    }
+
+  }
+
+  addInfoWindow(marker, content){
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+  loadMap(){
+
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 25000,
+      maximumAge: 0
+    };
+
+      let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 3,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.map.addListener('click', (event) => {
+        this.addMarker(event);
+      });
   }
 
 }
